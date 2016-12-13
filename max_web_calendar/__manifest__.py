7901c21d0bacb8d96610d5aa810e080fd847649a
zhoujunli <zhouhenry@live.com>
2016-12-13 22:53:05 +0800

    add web_calendar module

---
+++ max_web_calendar/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web Calendar Enhancement",
+
+    'summary': """
+        To enhance offical web_calendar mudule.
+    """,
+
+    'description': """
+        To enhance offical web_calendar mudule.
+    """,
+
+    'author': "MAXodoo",
+    'website': "http://www.maxodoo.com",
+    'category': 'web',
+    'version': '10.0.0.1',
+    'depends': ['base', 'web_calendar'],
+    'data': [
+        'views/max_web_calendar_view.xml',
+    ],
+}
