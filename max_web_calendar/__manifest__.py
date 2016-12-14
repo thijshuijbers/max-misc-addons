823c33faefa2116c4770fd166b4bb4e6af2f0987
zhoujunli <zhouhenry@live.com>
2016-12-14 11:04:05 +0800

    refine module description

--- max_web_calendar/__manifest__.py
+++ max_web_calendar/__manifest__.py
@@ -3,12 +3,13 @@
     'name': "Web Calendar Enhancement",
 
     'summary': """
-        To enhance offical web_calendar mudule.
+        To enhance official odoo web_calendar mudule.
     """,
 
     'description': """
-        To make specific color as color field setting of the event and
-        to support HTML tags in display setting of the event in calendar view.
+        Set certain color for events based on color field value.
+        Support HTML tags in display setting of the event in calendar view.
+        Fix translation issue of selection type fields.
     """,
 
     'author': "MAXodoo",
